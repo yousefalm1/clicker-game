73db7317e45c1452001e47d624cf6c55649141a6
Yousef Almesaeed <yousefalm@Outlook.com>
2024-10-17 13:00:30 +0300

    Finsihed set up count increase

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,26 @@
+'use client';
 import Image from 'next/image';
+import { useState } from 'react';
 
 const Home = () => {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div className="mx-auto">
       <div className=" flex justify-center mt-56 ">
-        <h1 className=" text-7xl ">1</h1>
+        <h1 className=" text-7xl ">{count}</h1>
       </div>
 
       <div className="flex justify-center">
-        <button className="py-5 bg-slate-800 px-8 rounded-full hover:bg-slate-900">
-          Click Me
+        <button
+          className="py-5 bg-slate-800 px-8 rounded-full hover:bg-slate-900 my-4"
+          onClick={handleClick}
+        >
+          Click me
         </button>
       </div>
     </div>
