c6ffddebaee892ec4e303a5760b26018e3fb8211
fajri1997 <f.a.f.s.m.alhusaini@edu.salford.ac.uk>
2024-10-20 11:13:36 +0100

    here

---
+++ src/app/data/Background.js
@@ -0,0 +1,44 @@
+const backgrounds = [
+  {
+    id: 1,
+    name: "Default",
+    className: "bg-black",
+    description: "The standard background with no color preference.",
+    cost: 0,
+    purchased: true,
+  },
+  {
+    id: 2,
+    name: "Red",
+    className: "bg-red",
+    description: "Choose the vibrant red background!",
+    cost: 500,
+    purchased: false,
+  },
+  {
+    id: 3,
+    name: "Blue",
+    className: "bg-blue",
+    description: "Select the calming blue background.",
+    cost: 1000,
+    purchased: false,
+  },
+  {
+    id: 4,
+    name: "Green",
+    className: "bg-green",
+    description: "Opt for the refreshing green background.",
+    cost: 1500,
+    purchased: false,
+  },
+  {
+    id: 5,
+    name: "Yellow",
+    className: "bg-yellow",
+    description: "Brighten your game with a cheerful yellow.",
+    cost: 2000,
+    purchased: false,
+  },
+];
+
+export default backgrounds;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,44 +1,43 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
+import { useState } from "react";
 
-import rewards from './data/rewards';
-import GoldCoinIcon from './assets/svg/gold-coin.svg';
+import rewards from "./data/rewards";
+import GoldCoinIcon from "./assets/svg/gold-coin.svg";
 
-import UpgradeShopContainer from './components/UpgradeShopContainer';
-import { NavBarContainer } from './components/NavBarContainer';
-import { ClickerIcon } from './components/ClickerIcon';
-import ClickCounter from './components/ClickCounter';
-import Modal from './components/Modal';
-import ProgressBar from './components/ProgressBar';
+import UpgradeShopContainer from "./components/UpgradeShopContainer";
+import { NavBarContainer } from "./components/NavBarContainer";
+import { ClickerIcon } from "./components/ClickerIcon";
+import ClickCounter from "./components/ClickCounter";
+import Modal from "./components/Modal";
+import ProgressBar from "./components/ProgressBar";
 
 const Home = () => {
-  // MOST IMPORTANT STATE
   const [count, setCount] = useState(0);
 
-  const [currentCurrency, setCurrentCurrency] = useState(0);
+  const [currentCurrency, setCurrentCurrency] = useState(500);
 
   const [currentMultiplier, setMultiplier] = useState(1);
 
-  // CLICKER STORAGE
   const [currentClickerStorage, setCurrentClickerStorage] = useState([
     {
-      id: 'default_clicker',
-      name: 'Gold Coin Clicker',
-      description: 'We all need a bit of luck to start!',
+      id: "default_clicker",
+      name: "Gold Coin Clicker",
+      description: "We all need a bit of luck to start!",
       cost: 0,
       IconComponent: GoldCoinIcon,
     },
   ]);
 
-  // WOULD LIKE TO CREDIT @mshll for help with active Clicker , the confetti and the product store alignment
   const [activeClicker, setActiveClicker] = useState(currentClickerStorage[0]);
 
   const [currentGoalCount, setCurrentGoalCount] = useState(rewards[0].count);
 
-  const [currentRewardDescription, setCurrentRewardDescription] = useState('');
+  const [currentRewardDescription, setCurrentRewardDescription] = useState("");
 
   const [showModal, setShowModal] = useState(false);
+
+  const [backgroundSettings, setBackgroundSettings] = useState("bg-black-500");
 
   const handleActiveClicker = (clicker) => {
     if (currentClickerStorage.includes(clicker)) setActiveClicker(clicker);
@@ -46,12 +45,10 @@ const Home = () => {
 
   const handleCloseModal = () => setShowModal(false);
 
-  // Helper Function To Calculate The Percentage
   const calculateProgress = (count, nextGoal) => {
     return nextGoal ? Math.min((count / nextGoal) * 100, 100) : 100;
   };
 
-  // Find the next goal
   const nextGoal = rewards.find((reward) => reward.count > count);
 
   const progress = calculateProgress(count, nextGoal && nextGoal.count);
@@ -61,7 +58,6 @@ const Home = () => {
       const newCount = count + currentMultiplier;
       let newCurrency = currentCurrency + currentMultiplier;
 
-      // Check if the user reaches a reward
       const reward = rewards.find((reward) => reward.count === newCount);
       if (reward) {
         newCurrency = newCurrency + reward.reward;
@@ -70,7 +66,6 @@ const Home = () => {
       }
       setCurrentCurrency(newCurrency);
 
-      // Update the next goal if needed
       const updatedGoal = rewards.find((reward) => reward.count > newCount);
       if (updatedGoal) {
         setCurrentGoalCount(updatedGoal.count);
@@ -81,20 +76,19 @@ const Home = () => {
   };
 
   return (
-    <div className="mx-auto">
+    <div className={`mx-auto ${backgroundSettings}`}>
+      {" "}
       <Modal
         showModal={showModal}
         currentRewardDescription={currentRewardDescription}
         handleCloseModal={handleCloseModal}
       />
-
       <NavBarContainer
         currentCurrency={currentCurrency}
         currentMultiplier={currentMultiplier}
         clickers={currentClickerStorage}
         handleActiveClicker={handleActiveClicker}
       />
-
       <UpgradeShopContainer
         setCurrentCurrency={setCurrentCurrency}
         currentCurrency={currentCurrency}
@@ -102,16 +96,15 @@ const Home = () => {
         currentMultiplier={currentMultiplier}
         setCurrentClickerStorage={setCurrentClickerStorage}
         currentClickerStorage={currentClickerStorage}
+        // backgroundSettings={backgroundSettings}
+        // setBackgroundSettings={setBackgroundSettings}
       />
-
       <ClickerIcon
         handleClick={handleClick}
         currentClickerStorage={currentClickerStorage}
         activeClicker={activeClicker}
       />
-
       <ClickCounter count={count} />
-
       <ProgressBar progress={progress} nextGoal={nextGoal} count={count} />
     </div>
   );
